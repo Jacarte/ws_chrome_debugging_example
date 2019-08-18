0f712fd0e63f5d966ce7a92347e7f17489ecd487
Javier Cabrera <javierca@kth.se>
2019-08-18 13:42:28 +0200

    Network logging example

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 const WebSocket = require('ws');
-const { exec, spawn } = require('child_process');
+
+// To open chrome as child process
+const {  spawn } = require('child_process');
+
 const req = require("request");
 var sleep = require('system-sleep')
-// Open browser in a clean session
+const fs = require("fs");
 
-/*
---no-sandbox --no-default-browser-check -user-data-dir=. 
---js-flags=" --print-bytecode"  (mutation-url|original-url) 
-& sleep 10; kill $!
-*/
-
-// ./Users/javiercabrera/Applications/Chromium.app/Contents/MacOS/Chrome
 
 const port = 9222;
-const waitFor = 5;// 5 seconds
+const waitFor = 3;// 5 seconds
 const timeout = (60 + waitFor)*1000; //10 seconds
 
-const chrome = spawn('/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome',[
+const chrome = spawn('chromium',[
   '--remote-debugging-port='+port,
   '--user-data-dir=demo/temp',
   process.argv[2]
 ]);
-
-
-console.log(Date.now())
 
 let interval = setTimeout(()=>{
   chrome.kill()
@@ -32,12 +25,17 @@ let interval = setTimeout(()=>{
 
 // Asking for opened tabs
 
-sleep(waitFor*1000)
-console.log(Date.now())
+sleep(waitFor)
 
+const log = fs.openSync("log.txt", 'a')
+
+// Accessing chrome publish websocket address
 req(`http://localhost:${port}/json`,function (error, response, body) {
     
   const tab = JSON.parse(body)[0];
+
+  console.log("Enabled websockets")
+  console.log(tab)
 
   const url = tab.webSocketDebuggerUrl;
 
@@ -46,19 +44,14 @@ req(`http://localhost:${port}/json`,function (error, response, body) {
 
   ws.on('open', function open() {
 
-    ws.send(JSON.stringify({id: 3, method: 'Debugger.enable'}))
-    ws.send(JSON.stringify({id: 1, method: 'Runtime.enable'}))
+    ws.send(JSON.stringify({id: 3, method: 'Network.enable'}))
 
-    ws.send(JSON.stringify({
-      id: 2,
-      method: 'Runtime.addBinding',
-      params: {
-        name: "eval"
-      }
-    }))
   });
-
+ 
   ws.on('message', function incoming(data) {
-    console.log(data);
+    
+    fs.writeSync(log, data + "\n")
+    
+    
   });
 });
